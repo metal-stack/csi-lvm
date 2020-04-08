b56055fc80148837bac49c080c2b75585d7ac631
Anton Leanenka <aleanenka@evolutiongaming.com>
2020-04-08 18:39:00 +0300

    add vgname flag and CSI_LVM_VG_NAME evn var for controller

--- cmd/controller/controller.go
+++ cmd/controller/controller.go
@@ -18,7 +18,6 @@ import (
 
 const (
 	keyNode          = "kubernetes.io/hostname"
-	typeAnnotation   = "csi-lvm.metal-stack.io/type"
 	linearType       = "linear"
 	stripedType      = "striped"
 	mirrorType       = "mirror"
@@ -42,10 +41,11 @@ type lvmProvisioner struct {
 	// defaultLVMType the lvm type to use by default if not overwritten in the pvc spec.
 	defaultLVMType string
 	pullPolicy     v1.PullPolicy
+	vgName         string
 }
 
 // NewLVMProvisioner creates a new lvm provisioner
-func NewLVMProvisioner(kubeClient clientset.Interface, namespace, lvDir, devicePattern, provisionerImage, defaultLVMType, pullPolicy string) controller.Provisioner {
+func NewLVMProvisioner(kubeClient clientset.Interface, namespace, vgName, lvDir, devicePattern, provisionerImage, defaultLVMType, pullPolicy string) controller.Provisioner {
 	pp := v1.PullAlways
 	if strings.ToLower(pullPolicy) == pullIfNotPresent {
 		pp = v1.PullIfNotPresent
@@ -57,6 +57,7 @@ func NewLVMProvisioner(kubeClient clientset.Interface, namespace, lvDir, deviceP
 		provisionerImage: provisionerImage,
 		kubeClient:       kubeClient,
 		namespace:        namespace,
+		vgName:           vgName,
 		defaultLVMType:   defaultLVMType,
 		pullPolicy:       pp,
 	}
@@ -94,6 +95,7 @@ func (p *lvmProvisioner) Provision(options controller.ProvisionOptions) (*v1.Per
 
 	lvmType := p.defaultLVMType
 
+	typeAnnotation := p.vgName + ".metal-stack.io/type"
 	userAnnotation, ok := options.PVC.Annotations[typeAnnotation]
 	if ok {
 		lvmType = userAnnotation
@@ -226,7 +228,7 @@ func (p *lvmProvisioner) createProvisionerPod(va volumeAction) (err error) {
 	if va.action == actionTypeDelete {
 		args = append(args, "deletelv")
 	}
-	args = append(args, "--lvname", va.name, "--vgname", "csi-lvm", "--directory", p.lvDir)
+	args = append(args, "--lvname", va.name, "--vgname", p.vgName, "--directory", p.lvDir)
 	if va.isBlock {
 		args = append(args, "--block")
 	}
@@ -237,7 +239,7 @@ func (p *lvmProvisioner) createProvisionerPod(va volumeAction) (err error) {
 	privileged := true
 	provisionerPod := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: string(va.action) + "-" + va.name,
+			Name: p.vgName + string(va.action) + "-" + va.name,
 		},
 		Spec: v1.PodSpec{
 			RestartPolicy: v1.RestartPolicyNever,
@@ -249,7 +251,7 @@ func (p *lvmProvisioner) createProvisionerPod(va volumeAction) (err error) {
 			},
 			Containers: []v1.Container{
 				{
-					Name:    "csi-lvm-" + string(va.action),
+					Name:    p.vgName + "-" + string(va.action),
 					Image:   p.provisionerImage,
 					Command: []string{"/csi-lvm-provisioner"},
 					Args:    args,
